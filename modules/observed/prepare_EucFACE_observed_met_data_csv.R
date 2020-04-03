e3c5ca25090a53deac421c0acf13aa721c0ab163
mingkaijiang <shanyujordan512@gmail.com>
2020-04-03 15:58:28 +1100

    working on soil data

--- modules/observed/prepare_EucFACE_observed_met_data_csv.R
+++ modules/observed/prepare_EucFACE_observed_met_data_csv.R
@@ -83,11 +83,120 @@ prepare_EucFACE_observed_met_data_csv <- function(timestep) {
     ### CO2 concentration in the rings
     myDF4 <- download_co2_data()
     
+    #### Assign ring information
+    myDF4$Ring <- sub("FACE_AUTO_R", "", myDF4$Source)
+    myDF4$Ring <- sub("_FCPLOGG.*", "", myDF4$Ring)
+    myDF4$Ring <- as.numeric(myDF4$Ring)  
     
-    #######################################################################################
+    ### assign data and time information
+    myDF4$YEAR <- year(myDF4$Date)
+    myDF4$DOY <- yday(myDF4$Date)
+    myDF4$Hour <- substr(myDF4$DateTime, start=12, stop=13)
+    myDF4$Minute <- substr(myDF4$DateTime, start=15, stop=16)
+    
+    ## ignore 2020 & 2011
+    myDF4 <- subset(myDF4, YEAR != "2020")
+    myDF4 <- subset(myDF4, YEAR != "2011")
+    
+    ## assign half hour 
+    myDF4$HalfHour <- ifelse(myDF4$Minute > 30, "30", "00")
+    
+    ## quality control
+    myDF4$WindSpeed <- as.numeric(as.character(myDF4$WindSpeed))
+    
+    myDF4$Concentration.1Min[myDF4$Concentration.1Min > 1000] <- 1000
+    myDF4$Concentration.1Min[myDF4$Concentration.1Min < 350] <- 350
+    
+    myDF4$WindSpeed[myDF4$WindSpeed < 0] <- 0.0
+    
+    myDF4$IRGA.Pressure[myDF4$IRGA.Pressure < 900] <- 900
+    myDF4$IRGA.Pressure[myDF4$IRGA.Pressure > 1100] <- 1100
+    
+    
+    ## half hourly data
+    outDF4 <- summaryBy(WindSpeed+Air.Temp+Concentration.1Min+IRGA.Vapor.Pressure+IRGA.Pressure+PPFD~Date+Hour+HalfHour+Ring, 
+                        FUN=mean,
+                        data=myDF4, keep.names=T, na.rm=T)
+    
+    
+    ### calculate RH
+    saturate.vp.func <- function(Tc,a=6.12,m=7.59,Tn=240.73){
+        # T = Temperature (°C)
+        VPS <- a*10^(m*Tc/(Tc+Tn))
+        return(VPS)
+    }
+    
+    outDF4$RH <- outDF4$IRGA.Vapor.Pressure/saturate.vp.func(outDF4$Air.Temp)
+    
+    
+    ### quality control
+    outDF4$Air.Temp[outDF4$Air.Temp > 55] <- 55
+    outDF4$RH[outDF4$RH > 100] <- 100
+    
+    ### averaging across rings
+    outDF5 <- summaryBy(WindSpeed+Air.Temp+IRGA.Pressure+PPFD+RH~Date+Hour+HalfHour,
+                        FUN=mean, data=outDF4, keep.names=T)
+    
+    outDF4$Trt <- "aCO2"
+    outDF4$Trt[outDF4$Ring%in%c(1,4,5)] <- "eCO2"
+    
+    outDF6 <- summaryBy(Concentration.1Min~Date+Hour+HalfHour+Trt,
+                        FUN=mean, data=outDF4, keep.names=T)
+    
+    subDF1 <- subset(outDF6, Trt=="aCO2")
+    subDF2 <- subset(outDF6, Trt=="eCO2")
+    outDF7 <- merge(subDF1, subDF2, by=c("Date", "Hour", "HalfHour"), all=T)
+    
+    ### merge the two datasets
+    outDF8 <- merge(outDF5, outDF7, by=c("Date", "Hour", "HalfHour"), all=T)
+    
+    outDF8$Trt.x <- NULL
+    outDF8$Trt.y <- NULL
+    
+    colnames(outDF8) <- c("Date", "Hour", "HalfHour", "WindSpeed", "Air.Temp",
+                          "IRGA.Pressure", "PPFD", "RH", "aCO2", "eCO2")
+    
+    ### merge all datasets to fill gaps in time coverage and data variables
+    outDF9 <- merge(outDF, outDF8, by=c("Date", "Hour", "HalfHour"), all=T)
+    
+    ### fill data gaps
+    outDF9$RH.y <- ifelse(is.na(outDF9$RH.y), outDF9$RH.x, outDF9$RH.y)
+    outDF9$RH.x <- NULL
+    
+    outDF9$WindSpeed <- ifelse(is.na(outDF9$WindSpeed), outDF9$WS_ms_Avg, outDF9$WindSpeed)
+    outDF9$WS_ms_Avg <- NULL
+    outDF9$wnd_spd <- NULL
+
+    outDF9$Air.Temp <- ifelse(is.na(outDF9$Air.Temp), outDF9$AirTC_Avg, outDF9$Air.Temp)
+    outDF9$AirTC_Avg <- NULL
+    outDF9$Ts_mean <- NULL
+    outDF9$TargTempC_Avg.1. <- NULL
+    
+    outDF9$PPFD <- ifelse(is.na(outDF9$PPFD), outDF9$PPFD_Avg, outDF9$PPFD)
+    outDF9$PPFD_Avg <- NULL
+    outDF9$LI190SB_PAR_Den_Avg <- NULL
+
+    outDF9$Net_SW_Avg <- NULL
+    outDF9$Net_LW_Avg <- NULL
+    outDF9$Net_Rad_Avg <- NULL
+    
+    outDF9$IRGA.Pressure <- ifelse(is.na(outDF9$IRGA.Pressure), outDF9$Pressure_hPa_Avg, outDF9$IRGA.Pressure)
+    outDF9$Pressure_hPa_Avg <- NULL
+    outDF9$Pressure_kPa <- NULL
+    outDF9$Pressure_Pa <- NULL
+    
+    
+    ### assign column names
+    colnames(outDF9) <- c("Date", "Hour", "HalfHour", "Rain", "SWnet", "LWnet",
+                          "Radnet", "Wind", "Tair", "PSurf", "PAR", "RH", "CO2ambient",
+                          "CO2elevated")
+    
     ### calculate VPD
-    outDF$VPD <- RHtoVPD(outDF$RH, outDF$AirTC_Avg)
+    outDF9$VPD <- RHtoVPD(outDF9$RH, outDF9$Tair)
     
+    ## add additional variables
+    outDF9$YEAR <- year(outDF9$Date)
+    outDF9$DOY <- yday(outDF9$Date)
     
     #######################################################################################
     ### read N deposition and CO2 data
@@ -95,10 +204,64 @@ prepare_EucFACE_observed_met_data_csv <- function(timestep) {
     colnames(ndepDF) <- c("YEAR", "DOY", "CO2air", "elevatedCO2", "Ndep")
     ndepDF$elevatedCO2 <- NULL
     ndepDF$Ndep <- ndepDF$Ndep / 10
-
     
     ### assign ndep data onto the outDF
+    outDF9 <- merge(outDF9, ndepDF, by=c("YEAR", "DOY"), all.x=T)
     
+    outDF9$CO2ambient <- ifelse(is.na(outDF9$CO2ambient), outDF9$CO2air, outDF9$CO2ambient)
+    outDF9$CO2elevated <- ifelse(is.na(outDF9$CO2elevated), outDF9$CO2air, outDF9$CO2elevated)
+    outDF9$CO2air <- NULL
+    
+    ### fill missing values
+    outDF9$Rain <- ifelse(is.na(outDF9$Rain), 0.0, outDF9$Rain)
+    outDF9$PSurf <- ifelse(is.na(outDF9$PSurf), 1015, outDF9$PSurf)
+    
+    ### shortwave radiation
+    outDF9$SWdown <- ifelse(outDF9$SWnet<=0, 0.0, outDF9$SWnet)
+    
+    ### longwave down 
+    outDF9$tairK <- outDF9$Tair + 273.15
+    
+    outDF9$sat_vapress <- 611.2 * exp(17.67 * ((outDF9$tairK - 273.15) / (outDF9$tairK - 29.65)))
+    outDF9$vapress <- max(5.0, outDF9$RH) / 100. * outDF9$sat_vapress
+    outDF9$LWdown <- 2.648 * outDF9$tairK + 0.0346 * outDF9$vapress - 474.0
+
+    ### fill missing values    
+    outDF9$SWdown <- na.locf(outDF9$SWdown)
+
+    ### delete unneeded variables
+    outDF9$sat_vapress <- NULL
+    outDF9$vapress <- NULL
+    outDF9$Radnet <- NULL
+    outDF9$SWnet <- NULL
+    outDF9$LWnet <- NULL
+    outDF9$tairK <- NULL
+    
+    ### unit
+    outDF9$Tair <- outDF9$Tair + 273.15
+    outDF9$PSurf <- outDF9$PSurf * 100
+    
+    #######################################################################################
+    ### EucFACE soil temperature data
+    soilDF <- download_soil_data()
+    
+    
+    ### assign data and time information
+    soilDF$YEAR <- year(soilDF$Date)
+    soilDF$DOY <- yday(soilDF$Date)
+    soilDF$Hour <- substr(soilDF$DateTime, start=12, stop=13)
+    soilDF$Minute <- substr(soilDF$DateTime, start=15, stop=16)
+    
+    ## ignore 2020 & 2011
+    soilDF <- subset(soilDF, YEAR != "2020")
+    soilDF <- subset(soilDF, YEAR != "2011")
+    
+    ## assign half hour 
+    myDF1$HalfHour <- ifelse(myDF1$Minute > 30, "30", "00")
+    
+    ## half hourly rainfall data
+    outDF1 <- summaryBy(Rain_mm_Tot~Date+Hour+HalfHour, FUN=sum,
+                        data=myDF1, keep.names=T, na.rm=T)
     #######################################################################################
     
     ### generate variable name and unit list
@@ -108,26 +271,7 @@ prepare_EucFACE_observed_met_data_csv <- function(timestep) {
     
     colnames(inDF2) <- var.list
     
-    
-    ### SWdown and PAR for some days are all zero, so replace these data with
-    ### data from one day earlier
-    missing.data.list <- data.frame(c("1992", "1996", "1997", "1998",
-                                      "1998", "1999", "2001", "2002",
-                                      "2003", "2004", "2005", "2006"),
-                                    c("351", "126", "280", "218", "219",
-                                      "91", "31", "49", "52", "275",
-                                      "181", "349"))
-    colnames(missing.data.list) <- c("YEAR", "DOY")
-    
-    for (i in missing.data.list$YEAR) {
-        for (j in missing.data.list$DOY) {
-            k <- as.numeric(j) - 1
-            inDF2[inDF2$YEAR==i&inDF2$DOY==j, "SWdown"] <- inDF2[inDF2$YEAR==i&inDF2$DOY==k, "SWdown"]
-            inDF2[inDF2$YEAR==i&inDF2$DOY==j, "PAR"] <- inDF2[inDF2$YEAR==i&inDF2$DOY==k, "PAR"]
-        }
-    }
 
-    
     ### add unit and name list
     unit.list <- c("year", "day", "hour", "W m-2", "umol m-2 s-1", "W m-2", "K", "kg m-2 s-1",
                    "Pa", "%", "m s-1", "Pa", "ppmv", "K", "g N m-2 yr-1")
