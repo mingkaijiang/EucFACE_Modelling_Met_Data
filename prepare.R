eca3331040e1c41e19fa472baaefab2fe1348fee
mingkaijiang <shanyujordan512@gmail.com>
2020-12-16 10:27:29 +1100

    unsuccessful

--- prepare.R
+++ prepare.R
@@ -75,8 +75,7 @@ pacman::p_load(dplyr,
                plantecophys,
                zoo,
                akima,
-               soilphysics,
-               HydroMe)    
+               soilphysics)    
 
 #### Sourcing all R files in the modules subdirectory
 sourcefiles <- dir("modules", pattern="[.]R$", recursive = TRUE, full.names = TRUE)
